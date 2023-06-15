e6d81cc2787cc20221e1ba4c7a5015da24b025e9
Albertin38 <miguelalbert38@gmail.com>
2023-06-15 01:15:03 -0300

    ?

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
+import { Modal } from './modal.js'
+
 // variables
 const form = document.querySelector('form');
 const inputWeigth = document.querySelector('#weigth');
 const inputHeight = document.querySelector('#height');
-
-// const modalWrapper = document.querySelector('.modal-wrapper');
-// const modalMessage = document.querySelector('.modal .title span');
-// const modalBtnClose = document.querySelector('.modal button.close');
-
-const Modal = {
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open() {
-        Modal.wrapper.classList.add('open')
-    },
-    close() {
-        Modal.wrapper.classList.remove('open')
-    },
-
-}
 
 // Eventos
 form.onsubmit = (event) => {
@@ -34,10 +18,7 @@ form.onsubmit = (event) => {
     Modal.message.innerText = message;
     // modalWrapper.classList.add('open')
     Modal.open();
-
 }
-
-Modal.buttonClose.onclick = () => Modal.close()
 
 // funções
 function IMC(weight, height) {
--- modal.js
+++
@@ -1,3 +0,0 @@
-export 
-
-Modal.buttonClose.onclick = () => Modal.close()
